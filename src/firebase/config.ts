0813d1de434520c9d59bebd15d4d1128baf948d1
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2025-11-26 02:42:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3200695440-bed4a",
+  "appId": "1:490800932050:web:4cbdeaf3b48a840ac9790b",
+  "apiKey": "AIzaSyDFdA0bvqKUsINav1zeej0qrC8VNz2n1Wk",
+  "authDomain": "studio-3200695440-bed4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "490800932050"
+};
