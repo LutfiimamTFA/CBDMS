a51aa7e1aa2f940b641d60640317f267d7e7fdac
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2025-12-18 01:37:57 +0000

    eksekusi

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -167,7 +167,7 @@ const Sidebar = React.forwardRef<
     {
       side = "left",
       variant = "sidebar",
-      collapsible = "offcanvas",
+      collapsible = "icon",
       className,
       children,
       ...props
