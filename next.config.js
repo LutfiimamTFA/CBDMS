30c35e64c1a3565a2164555a74c52e73e7c07223
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2026-01-30 04:24:01 +0000

    perbaiki

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 
 
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
   typescript: {
     ignoreBuildErrors: true,
   },
